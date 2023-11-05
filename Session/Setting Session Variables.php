e1859d260b2d137b0d02d755bc3091ce7889b4e7
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-05 17:05:22 +0800

    changing

--- Session/Setting Session Variables.php
+++ Session/Setting Session Variables.php
@@ -3,8 +3,8 @@
 session_start();
 
 // Set session variables
-$_SESSION["userId"] = 2136991;
-$_SESSION["userName"] = "Jhon Mark";
+$_SESSION["userId"] = 901010;
+$_SESSION["userName"] = "Hatdog";
 $_SESSION["Age"] = "20";
 
 echo "Session variables set successfully.";
