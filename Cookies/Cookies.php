d9b0646ec7cc948b658c44bb83e54e49921c3e9d
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-05 17:05:54 +0800

    update

--- Cookies/Cookies.php
+++ Cookies/Cookies.php
@@ -1,9 +1,9 @@
 <?php
 // Set a cookie named "userList" with the value "Student Name" and an expiration time of 3.5 hours
-setcookie("userList", "Jhon Mark", time() + 3.5 * 3600);
+setcookie("userList", "Yow", time() + 3.5 * 3600);
 
 // Set a cookie named "userCourse" with the value "IT" and the same expiration time
-setcookie("userCourse", "IT-NT", time() + 3.5 * 3600);
+setcookie("userCourse", "IT", time() + 3.5 * 3600);
 
 echo "Cookies set successfully.";
 ?>
