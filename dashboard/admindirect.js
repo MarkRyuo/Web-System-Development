f37b2869f82b46ce254a31ceb15234c16f6edf5e
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-05 21:01:44 +0800

    update here

--- dashboard/admindirect.js
+++ dashboard/admindirect.js
@@ -1,7 +1,7 @@
 
 
   function redirectTouserprofile() {
-    window.location.href = '/UserProfile/Userprofile.php'; // Todo actual file path
+    window.location.href = './Userprofile.php'; // Todo actual file path
   }
 
   function redirectToproducts() {
