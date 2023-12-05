87459f841f9320f84ce52ac27798aba4039c74ca
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-05 21:43:28 +0800

    update herE

--- Usersssssss/adminprofile.php
+++ Usersssssss/adminprofile.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ims_db";
+$dbname = "db_nt3102";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -13,33 +13,32 @@ if ($conn->connect_error) {
 }
 
 if (!isset($_SESSION['username'])) {
-  header('Location: /Usersssssss/staffprofile.php');
+  header('Location: userProfile.php');
   exit;
 }
 
-// if ($_SESSION['role'] !== 'admin') {
-//   // Redirect if the user is not an admin
-//   header('Location: /UserProfile Security/userProfileSecurity.php');
-//   exit;
-// }
+if ($_SESSION['role'] !== 'admin') {
+  // Redirect if the user is not an admin
+  header('Location: /UserProfile Security/userProfileSecurity.php');
+  exit;
+}
 
 // Retrieve user information from the session
 $username = $_SESSION['username'];
-$role = $_SESSION['role'];
+$usersign = $_SESSION['usersign'];
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>staff</title>
+  <title>admin</title>
 </head>
 <body>
 
-  <form class="input">
-    <h1>Welcome staff!</h1>
+<form class="input">
+    <h1>Welcome admin!</h1>
 
     <label for="role" class="role">User Role:</label>
     <input type="text" id="role" name="role" value="<?php echo htmlspecialchars($role); ?>" readonly>
