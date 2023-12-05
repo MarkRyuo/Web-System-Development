02ad06075539b05d0b768081b27632e205177c28
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-05 20:45:34 +0800

    update here

--- dashboard/direct.js
+++ dashboard/direct.js
@@ -1,20 +1,20 @@
 
 
   function redirectTouserprofile() {
-    window.location.href = '/User Profile/Userprofile.php'; // Todo actual file path
+    window.location.href = '/UserProfile/Userprofile.php'; // Todo actual file path
   }
 
   function redirectToproducts() {
-    window.location.href = '/Login/login.html'; // Todo actual file path
+    window.location.href = ''; // Todo actual file path
   }
 
 
   function redirectviewtransacrtion() {
-      window.location.href = '/Login/login.html'; // Todo actual file path
+      window.location.href = ''; // Todo actual file path
     }
 
   function redirectTosuppliers() {
-    window.location.href = '/Login/login.html'; // Todo actual file path
+    window.location.href = ''; // Todo actual file path
   }
 
 
