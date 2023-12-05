4131a9202b3c0f6d4dcd51924a08f92cc08fc9e8
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-05 10:38:16 +0800

    update here

---
+++ User Profile/profile.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+include('config.php');
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$username = $_SESSION['username'];
+
+$sql = "SELECT * FROM users WHERE username='$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $id = $row['id'];
+    $role = $row['role'];
+} else {
+    echo "Error fetching user data.";
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+    <h1>Welcome, <?php echo $username; ?>!</h1>
+    <p>User ID: <?php echo $id; ?></p>
+    <p>Role: <?php echo $role; ?></p>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
