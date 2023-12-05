8a555d5a82fb134cd17f153cdcc60be2b4414ee3
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-05 09:56:13 +0800

    update here

--- dashboard/direct.js
+++ dashboard/direct.js
@@ -17,3 +17,21 @@
     window.location.href = '/Login/login.html'; // Todo actual file path
   }
 
+
+  // This is Btn Logout 
+
+
+  function confirmLogout() {
+    // Display a confirmation dialog
+    var result = window.confirm("Are you sure you want to logout?");
+
+    // Check the user's choice
+    if (result) {
+      // User clicked "OK" (Yes), redirect to a specific URL
+      alert("You have been logged out!");
+      window.location.href = "/Login/login.html";  
+    } else {
+      // User clicked "Cancel", do nothing or handle accordingly
+    }
+  }
+
