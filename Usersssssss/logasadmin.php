14551f91930bdb033b74dc7b6d1ef6c5e3f58c02
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-05 21:34:10 +0800

    update here

---
+++ Usersssssss/logasadmin.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+require_once('config.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare('SELECT * FROM users WHERE username = ? AND password = ?');
+    $stmt->bind_param('ss', $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
+        // Assuming you retrieve full name from the database query
+        // $_SESSION['usersign'] = $user['usersign'];
+
+
+        // Redirect based on the user's role
+        if ($user['role'] === 'admin') {
+            header('Location: ../User-Profile/userProfile.php');
+        } else {
+            header('Location: ../UserProfile Security/userProfileSecurity.php');
+        }
+        exit;
+    } else {
+        echo "Invalid username or password";
+    }
+
+    $stmt->close();
+}
+?>
+
