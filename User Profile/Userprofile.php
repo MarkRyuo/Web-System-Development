5004e2468808044a18364ad0ebe7f7b2014c49fb
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-05 19:28:22 +0800

    update here

--- User Profile/Userprofile.php
+++ User Profile/Userprofile.php
@@ -12,44 +12,44 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if (!isset($_SESSION['username'])) {
-  header('Location: Userprofile.php');
-  exit;
-}
-
-if ($_SESSION['role'] !== 'admin') {
-  // Redirect if the user is not an admin
-  header('Location: /dashboard/Dashboard.html');
-  exit;
+if (!isset($_SESSION['username']) || !isset($_SESSION['role'])) {
+    header('Location: Userprofile.php');
+    exit;
 }
 
 // Retrieve user information from the session
-$username = $_SESSION['username'];
-$role = $_SESSION['role'];
+$username = htmlspecialchars($_SESSION['username']);
+$role = htmlspecialchars($_SESSION['role']);
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome <?php echo ucfirst($role); ?></title>
 </head>
 <body>
 
-
-<form class="input" >
+<form class="input">
+    <?php if ($role === 'admin'): ?>
         <h1>Welcome Admin!</h1>
-
-        <label for="role" class="role">User:</label>
-        <input type="text" id="role" name="role" value="<?php echo htmlspecialchars($role); ?>" readonly>
-
+        <!-- Admin-specific content goes here -->
+        <label for="role" class="role">User Role:</label>
+        <input type="text" id="role" name="role" value="<?php echo $role; ?>" readonly>
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username" value="<?php echo htmlspecialchars($username); ?>" readonly>
-      </form>
+        <input type="text" id="username" name="username" value="<?php echo $username; ?>" readonly>
+        <!-- Admin-specific content goes here -->
+    <?php else: ?>
+        <h1>Welcome Staff!</h1>
+        <!-- Staff-specific content goes here -->
+        <label for="role" class="role">User Role:</label>
+        <input type="text" id="role" name="role" value="<?php echo $role; ?>" readonly>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" value="<?php echo $username; ?>" readonly>
+        <!-- Staff-specific content goes here -->
+    <?php endif; ?>
+</form>
 
-    </section>
-  
 </body>
 </html>
